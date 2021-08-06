52774479363c8e40fd3b9adb756e8f33548f17e2
MashMaleLLO <74470396+MashMaleLLO@users.noreply.github.com>
2021-08-06 11:32:16 +0700

    learn more about where method

--- select_whereMethod.sql
+++ select_whereMethod.sql
@@ -3,8 +3,16 @@
 -- WHERE CustomerId = 1;
 -- SELECT * FROM customers
 -- WHERE CustomerId = 8;
-SELECT 
-	FirstName,
-	LastName 
-FROM customers
-WHERE CustomerId = 4;
+-- SELECT 
+-- 	FirstName,
+-- 	LastName 
+-- FROM customers
+-- WHERE CustomerId = 4;
+-- เลือกเฉพาะ by keyword
+-- SELECT * FROM customers
+-- WHERE Country = 'Canada';
+-- เลือกเฉพาะ by keyword รวมกับ expression
+-- SELECT * FROM customers
+-- WHERE Country = 'Canada' AND State = 'ON';
+SELECT * FROM customers
+WHERE Country = 'Canada' OR Country = 'Norway';
