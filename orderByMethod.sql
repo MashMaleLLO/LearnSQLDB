a8943cfb1414131ccda9e8a3199bcdbe2c730349
MashMaleLLO <74470396+MashMaleLLO@users.noreply.github.com>
2021-08-06 11:43:52 +0700

    learn about how to limit a table data and descended data

--- orderByMethod.sql
+++ orderByMethod.sql
@@ -1,4 +1,17 @@
 --เรียงข้อมูล โดยคำสั่ง order by
+--เรียงแบบ น้อยไปมาก
+-- SELECT * FROM customers
+-- WHERE Country = 'Canada' OR Country = 'USA'
+-- ORDER BY FirstName;
+--เรียงจากมากไปน้อย
+-- SELECT * FROM customers
+-- WHERE Country = 'Canada' OR Country = 'USA'
+-- ORDER BY FirstName DESC;
+-- SELECT * FROM customers
+-- WHERE Country = 'Canada' OR Country = 'USA'
+-- ORDER BY CustomerId DESC;
+--การเรียงและจำกัดข้อมูล
 SELECT * FROM customers
 WHERE Country = 'Canada' OR Country = 'USA'
-ORDER BY FirstName;
+ORDER BY CustomerId DESC
+LIMIT 5;
