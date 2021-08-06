19860cb154f7c2244c7e4704974a157da468cd39
MashMaleLLO <74470396+MashMaleLLO@users.noreply.github.com>
2021-08-06 18:42:51 +0700

    learn insert method

---
+++ insertMethod.sql
@@ -0,0 +1,8 @@
+--insert ข้อมูล
+--แบบง่าย insert ตามลำดับของ field
+-- INSERT INTO customers
+-- VALUES(60,"Narawich","Youbua","KMITL","NON","BK","BK","TH",11130,
+-- 0924637761,NULL,"narawich49@gmail.com",NULL);
+--อีกวิธี
+INSERT INTO customers(FirstName, LastName, Email)
+VALUES("Phinlada","Youbua","62010465@kmitl.com")
