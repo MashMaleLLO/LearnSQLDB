4268a34fe1635f23328cff1b9386bffdfcdc5d2d
MashMaleLLO <74470396+MashMaleLLO@users.noreply.github.com>
2021-08-07 22:19:31 +0700

    learn about join method

---
+++ joinMethod.sql
@@ -0,0 +1,8 @@
+--การ join ข้อมูล 2 อันหรือมากกว่ามาจอยกัน
+--inner join A(intersect)B
+-- SELECT invoices.InvoiceId, customers.FirstName , customers.LastName, invoices.Total AS "money"
+-- FROM invoices
+-- INNER JOIN customers ON customers.CustomerId = invoices.CustomerId;
+--left outer join B'(union)(A(INTERSECT)B)
+--Right outer join A'(union)(A(INTERSECT)B)
+
