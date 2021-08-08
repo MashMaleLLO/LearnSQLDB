c906586d5ec5bbad75dde12d0b5ea0f0531d0bc6
MashMaleLLO <74470396+MashMaleLLO@users.noreply.github.com>
2021-08-09 01:03:30 +0700

    edit join

--- joinPart2.sql
+++ joinPart2.sql
@@ -1,4 +1,6 @@
 --join applied
-SELECT employees.LastName,employees.FirstName FROM employees
-INNER JOIN employees AS employees2
-ON employees.EmployeeId = employees.ReportsTo;
+SELECT employees.EmployeeId, employees.FirstName, employees.LastName,
+employees.Title,employees2.FirstName AS FTNemployer,employees2.LastName AS LTNemployer
+FROM employees
+LEFT JOIN employees AS employees2
+ON employees.ReportsTo = employees2.EmployeeId;
